6a1424c9fb3aa6321a1cacc474b5fc7286b5b80c
Meiiiii <hoangsaomai04@gmail.com>
2021-12-20 21:40:04 -0800

    btl

---
+++ BaiTapBuoi7/bai1.js
@@ -0,0 +1,12 @@
+// Câu 1: Cho một chuỗi str, tìm ký tự không lặp lại đầu tiên trong chuỗi đó và trả về chỉ số của nó.Nếu nó không tồn tại, trả về -1.
+// case1
+// Input: str = "tranduong"
+// Output: 0
+// case2
+// Input: str = "hitclubhiuhiu"
+// Output: 2
+// case3
+// Input: str = "aabb"
+// Output: -1
+console.log(a);
+var a=2;
